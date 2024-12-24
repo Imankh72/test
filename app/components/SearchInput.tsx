d1fc183b3f16bef99a04b3e8aca25125a48f196f
iman <imankhorshidi72@gmail.com>
2024-12-24 20:45:10 +0330

    Fix: SearchInputComponent

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -5,7 +5,7 @@ const SearchInput = () => {
     <div className="relative h-9 flex justify-center lg:hidden">
       <input
         type="text"
-        className="bg-[#080F21] w-[60%] inline-block h-full rounded-[35px]"
+        className="bg-[#080F21] w-[60%] inline-block h-full rounded-[35px] text-white pr-7"
       />
       <div className="absolute top-1/2 right-12 -translate-y-1/2">
         <SearchInputIcon />
