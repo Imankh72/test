2e605b1807bd846739df7de712d0e92f7aabeb3b
iman <imankhorshidi72@gmail.com>
2024-12-24 10:25:20 +0330

    Add: Header Component

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+import SearchInput from "./SearchInput";
+
+const Header = () => {
+  return (
+    <header className="bg-[#262630] h-[145px] flex justify-center items-center lg:bg-white">
+      <SearchInput />
+    </header>
+  );
+};
+
+export default Header;
