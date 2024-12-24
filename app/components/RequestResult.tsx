462f975cd0f47b8a73fd6ba9f6ba7c74c89c24d5
iman <imankhorshidi72@gmail.com>
2024-12-25 00:09:28 +0330

    Fix: RequestResult Component

--- app/components/RequestResult.tsx
+++ app/components/RequestResult.tsx
@@ -2,7 +2,7 @@ import { BoxIcon } from "./svgs/BoxIcon";
 
 const RequestResult = () => {
   return (
-    <div className="bg-green-secondary text-green-primary mx-8 rounded-md py-5 px-4 lg:bg-white lg:py-8 lg:px-7 lg:text-dark-primary lg:flex lg:items-center lg:gap-3">
+    <div className="bg-green-secondary text-green-primary mx-8 rounded-md py-5 px-4 lg:bg-white lg:py-8 lg:px-8 lg:text-dark-primary lg:flex lg:items-center lg:gap-3">
       <div className="hidden lg:block">
         <BoxIcon />
       </div>
