0b8becdef44f55e9dc931c404bfc36558675c1ca
iman <imankhorshidi72@gmail.com>
2024-12-24 10:25:31 +0330

    Add: SearchInput Component

---
+++ app/components/SearchInput.tsx
@@ -0,0 +1,10 @@
+const SearchInput = () => {
+  return (
+    <input
+      type="text"
+      className="bg-[#080F21] inline-block h-9 rounded-[35px]"
+    />
+  );
+};
+
+export default SearchInput;
