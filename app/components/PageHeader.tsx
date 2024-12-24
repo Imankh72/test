373084f07985caf1c9d78b0e540174ad5779304f
iman <imankhorshidi72@gmail.com>
2024-12-24 20:44:51 +0330

    Fix: PageHeader Component

--- app/components/PageHeader.tsx
+++ app/components/PageHeader.tsx
@@ -8,13 +8,13 @@ const PageHeader = () => {
       <div className="flex flex-col items-center gap-3 bg-[#262630] size-full pt-3 pb-10 lg:bg-white lg:flex-row lg:justify-start">
         <SearchInput />
         <div className="flex justify-center items-center gap-2 lg:justify-start">
-          <p className="text-white text-xl tracking-widest font-bold lg:hidden">
+          <p className="font-inter text-white text-xl tracking-widest font-bold lg:hidden">
             ZARVANTRIP
           </p>
           <span className="inline-block bg-[#48486A] h-6 w-[0.125rem] rounded-[11px] lg:hidden"></span>
           <HeroIcon />
           <div className="hidden lg:flex flex-col items-center gap-1">
-            <p className="text-xl font-bold">ZARVANTRIP</p>
+            <p className="font-inter text-xl font-bold">ZARVANTRIP</p>
             <p className="font-mani">هتلداری زروان تریپ</p>
           </div>
         </div>
