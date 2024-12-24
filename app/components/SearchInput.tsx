7509839815dd822db38e9b18d9a453ff649fcd55
iman <imankhorshidi72@gmail.com>
2024-12-24 11:31:35 +0330

    Fix: SearchInput Component

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -2,12 +2,12 @@ import SearchInputIcon from "./svgs/SearchInputIcon";
 
 const SearchInput = () => {
   return (
-    <div className="relative h-9">
+    <div className="relative h-9 flex justify-center">
       <input
         type="text"
-        className="bg-[#080F21] inline-block h-full rounded-[35px]"
+        className="bg-[#080F21] w-[60%] inline-block h-full rounded-[35px]"
       />
-      <div className="absolute top-1/2 right-3 -translate-y-1/2">
+      <div className="absolute top-1/2 right-12 -translate-y-1/2">
         <SearchInputIcon />
       </div>
     </div>
