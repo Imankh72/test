3de684ea87e032e900aab5c77dac24a676caf3e8
iman <imankhorshidi72@gmail.com>
2024-12-24 13:02:30 +0330

    Fix: Tailwind Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        mani: ["var(--font-mani)"],
+        inter: ["var(--font-inter)"],
+      },
     },
   },
   plugins: [],
