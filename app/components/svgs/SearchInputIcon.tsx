bd1169f84cadeeffba87bbf8025208d6f0183747
iman <imankhorshidi72@gmail.com>
2024-12-24 11:06:41 +0330

    Add: SearchInputIcon Component

---
+++ app/components/svgs/SearchInputIcon.tsx
@@ -0,0 +1,74 @@
+const SearchInputIcon = () => {
+  return (
+    <svg
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g opacity="0.7">
+        <path
+          d="M12.5 23.326C18.6586 23.326 23.6608 18.3749 23.6608 12.2554C23.6608 6.13597 18.6586 1.18489 12.5 1.18489C6.34144 1.18489 1.33918 6.13597 1.33918 12.2554C1.33918 18.3749 6.34144 23.326 12.5 23.326Z"
+          fill="url(#paint0_radial_1_481)"
+        />
+        <path
+          d="M12.5 23.326C18.6586 23.326 23.6608 18.3749 23.6608 12.2554C23.6608 6.13597 18.6586 1.18489 12.5 1.18489C6.34144 1.18489 1.33918 6.13597 1.33918 12.2554C1.33918 18.3749 6.34144 23.326 12.5 23.326Z"
+          fill="url(#paint1_angular_1_481)"
+        />
+        <path
+          d="M12.5 23.326C18.6586 23.326 23.6608 18.3749 23.6608 12.2554C23.6608 6.13597 18.6586 1.18489 12.5 1.18489C6.34144 1.18489 1.33918 6.13597 1.33918 12.2554C1.33918 18.3749 6.34144 23.326 12.5 23.326Z"
+          fill="url(#paint2_angular_1_481)"
+        />
+        <path
+          d="M12.5 23.326C18.6586 23.326 23.6608 18.3749 23.6608 12.2554C23.6608 6.13597 18.6586 1.18489 12.5 1.18489C6.34144 1.18489 1.33918 6.13597 1.33918 12.2554C1.33918 18.3749 6.34144 23.326 12.5 23.326Z"
+          stroke="#080F21"
+          stroke-width="1.32163"
+        />
+      </g>
+      <defs>
+        <radialGradient
+          id="paint0_radial_1_481"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(12.5 12.2554) rotate(90) scale(10.4097 10.5)"
+        >
+          <stop offset="0.213542" />
+          <stop offset="0.270833" stop-color="#081A32" />
+          <stop offset="0.395833" stop-color="#1F3A58" />
+          <stop offset="0.515625" stop-color="#375F90" />
+          <stop offset="0.713542" stop-color="#274871" />
+          <stop offset="0.916667" stop-color="#0D1528" />
+        </radialGradient>
+        <radialGradient
+          id="paint1_angular_1_481"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(12.5 12.2554) rotate(90) scale(10.4097 10.5)"
+        >
+          <stop offset="0.411458" stop-opacity="0" />
+          <stop offset="0.510417" />
+          <stop offset="0.817708" stop-opacity="0" />
+        </radialGradient>
+        <radialGradient
+          id="paint2_angular_1_481"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(12.5 12.2554) rotate(90) scale(10.4097 10.5)"
+        >
+          <stop />
+          <stop offset="0.166667" stop-opacity="0" />
+          <stop offset="0.885417" stop-opacity="0" />
+        </radialGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default SearchInputIcon;
