097039940136f5b95cad94b0236561b82e2b0050
iman <imankhorshidi72@gmail.com>
2024-12-24 13:02:53 +0330

    Fix: PageHeader Component

--- app/components/PageHeader.tsx
+++ app/components/PageHeader.tsx
@@ -3,14 +3,20 @@ import HeroIcon from "./svgs/HeroIcon";
 
 const PageHeader = () => {
   return (
-    <header className="bg-[#262630] h-36 flex flex-col items-center gap-3 pt-3 lg:bg-white">
-      <SearchInput />
-      <div className="flex justify-center items-center gap-2">
-        <HeroIcon />
-        <span className="inline-block bg-[#48486A] h-6 w-[0.125rem] rounded-[11px]"></span>
-        <p className="text-white text-xl tracking-widest font-bold">
-          ZARVANTRIP
-        </p>
+    <header>
+      <div className="flex flex-col items-center gap-3 bg-[#262630] size-full pt-3 pb-10 lg:bg-white">
+        <SearchInput />
+        <div className="flex justify-center items-center gap-2">
+          <p className="text-white text-xl tracking-widest font-bold">
+            ZARVANTRIP
+          </p>
+          <span className="inline-block bg-[#48486A] h-6 w-[0.125rem] rounded-[11px] lg:hidden"></span>
+          <HeroIcon />
+          <div className="hidden lg:flex flex-col items-center gap-1">
+            <p className="text-xl font-bold">ZARVANTRIP</p>
+            <p className="font-mani">هتلداری زروان تریپ</p>
+          </div>
+        </div>
       </div>
     </header>
   );
