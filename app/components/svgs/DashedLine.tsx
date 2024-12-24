f35476a6d437937fd913cf3cd404fbfbcdee444f
iman <imankhorshidi72@gmail.com>
2024-12-24 20:46:25 +0330

    Add: DashedLine Component

---
+++ app/components/svgs/DashedLine.tsx
@@ -0,0 +1,21 @@
+const DashedLine = () => {
+  return (
+    <svg
+      width="308"
+      height="2"
+      viewBox="0 0 308 2"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M307 1.00003L1 1"
+        stroke="#1E8E29"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeDasharray="12 12"
+      />
+    </svg>
+  );
+};
+
+export default DashedLine;
