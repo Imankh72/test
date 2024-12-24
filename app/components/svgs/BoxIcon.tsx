797bf2b4827678d8f8e3d90e606c47a015584931
iman <imankhorshidi72@gmail.com>
2024-12-24 21:36:48 +0330

    Add: BoxIcon Component

---
+++ app/components/svgs/BoxIcon.tsx
@@ -0,0 +1,88 @@
+export const BoxIcon = () => {
+  return (
+    <svg
+      width="89"
+      height="81"
+      viewBox="0 0 89 81"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect y="11" width="75" height="70" rx="10" fill="#1E8E29" />
+      <g filter="url(#filter0_b_1_437)">
+        <rect
+          x="11"
+          width="78"
+          height="70"
+          rx="10"
+          fill="url(#paint0_linear_1_437)"
+          fillOpacity="0.4"
+        />
+        <rect
+          x="12"
+          y="1"
+          width="76"
+          height="68"
+          rx="9"
+          stroke="url(#paint1_linear_1_437)"
+          strokeWidth="2"
+        />
+      </g>
+      <path
+        opacity="0.5"
+        d="M47.9167 19.0832C49.0667 18.0999 50.95 18.0999 52.1167 19.0832L54.75 21.3499C55.25 21.7832 56.1833 22.1332 56.85 22.1332H59.6833C61.45 22.1332 62.9 23.5832 62.9 25.3499V28.1832C62.9 28.8332 63.25 29.7832 63.6833 30.2832L65.95 32.9165C66.9333 34.0665 66.9333 35.9499 65.95 37.1165L63.6833 39.7499C63.25 40.2499 62.9 41.1832 62.9 41.8499V44.6832C62.9 46.4499 61.45 47.8999 59.6833 47.8999H56.85C56.2 47.8999 55.25 48.2499 54.75 48.6832L52.1167 50.9499C50.9667 51.9332 49.0833 51.9332 47.9167 50.9499L45.2833 48.6832C44.7833 48.2499 43.85 47.8999 43.1833 47.8999H40.3C38.5333 47.8999 37.0833 46.4499 37.0833 44.6832V41.8332C37.0833 41.1832 36.7333 40.2499 36.3167 39.7499L34.0667 37.0999C33.1 35.9499 33.1 34.0832 34.0667 32.9332L36.3167 30.2832C36.7333 29.7832 37.0833 28.8499 37.0833 28.1999V25.3332C37.0833 23.5665 38.5333 22.1165 40.3 22.1165H43.1833C43.8333 22.1165 44.7833 21.7665 45.2833 21.3332L47.9167 19.0832Z"
+        fill="white"
+      />
+      <path
+        d="M47.9833 40.2831C47.65 40.2831 47.3333 40.1498 47.1 39.9165L43.0667 35.8831C42.5833 35.3998 42.5833 34.5998 43.0667 34.1165C43.55 33.6331 44.35 33.6331 44.8333 34.1165L47.9833 37.2665L55.15 30.0998C55.6333 29.6165 56.4333 29.6165 56.9167 30.0998C57.4 30.5831 57.4 31.3831 56.9167 31.8665L48.8667 39.9165C48.6333 40.1498 48.3167 40.2831 47.9833 40.2831Z"
+        fill="white"
+      />
+      <defs>
+        <filter
+          id="filter0_b_1_437"
+          x="-19"
+          y="-30"
+          width="138"
+          height="130"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feGaussianBlur in="BackgroundImageFix" stdDeviation="15" />
+          <feComposite
+            in2="SourceAlpha"
+            operator="in"
+            result="effect1_backgroundBlur_1_437"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_backgroundBlur_1_437"
+            result="shape"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_1_437"
+          x1="85.6452"
+          y1="-8.99649e-07"
+          x2="15.6568"
+          y2="71.2784"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#DEDEDE" />
+          <stop offset="1" stopColor="white" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_1_437"
+          x1="13.9355"
+          y1="67.1176"
+          x2="74.9605"
+          y2="10.0069"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFFDFD" />
+          <stop offset="1" stopColor="#EFEFEF" stopOpacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
