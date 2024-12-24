cb03faa9f4c8b7ea5bc6167c448c5f325f012ae1
iman <imankhorshidi72@gmail.com>
2024-12-25 01:15:43 +0330

    Add: HotelInformation Component

---
+++ app/components/HotelInformation.tsx
@@ -0,0 +1,160 @@
+const HotelInformation = () => {
+  return (
+    <div className="bg-white mx-8 rounded-md py-5 px-4">
+      <div className="py-5 px-4 bg-white rounded-md grid grid-cols-2 grid-rows-4 gap-x-12 gap-y-6 lg:px-8 lg:w-full lg:grid-cols-3 lg:gap-y-12">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+                هتل انتخاب شده :
+              </p>
+              <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+            <div className="relative">
+              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+                پارسیان آزادی تهران
+              </p>
+              <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+                مدت اقامت :
+              </p>
+              <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+            <div className="relative">
+              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+                3 شب
+              </p>
+              <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center row-start-3 row-end-4 col-start-1 col-end-3 lg:col-end-2">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+                مدت اقامت :
+              </p>
+              <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+            <div className="relative">
+              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+                3 شب
+              </p>
+              <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center row-start-4 row-end-5 col-start-1 col-end-3">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+                مدت اقامت :
+              </p>
+              <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+            <div className="relative">
+              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+                3 شب
+              </p>
+              <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+                مدت اقامت :
+              </p>
+              <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+            <div className="relative">
+              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+                3 شب
+              </p>
+              <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+                مدت اقامت :
+              </p>
+              <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+            <div className="relative">
+              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+                3 شب
+              </p>
+              <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+                مدت اقامت :
+              </p>
+              <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+            <div className="relative">
+              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+                3 شب
+              </p>
+              <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+                مدت اقامت :
+              </p>
+              <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+            <div className="relative">
+              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+                3 شب
+              </p>
+              <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <div className="relative">
+              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+                مدت اقامت :
+              </p>
+              <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+            <div className="relative">
+              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+                3 شب
+              </p>
+              <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <textarea
+        name=""
+        id=""
+        className="bg-gray-secondary inline-block p-3 w-full placeholder:text-dark-primary placeholder:text-xs placeholder:font-bold resize-none rounded-md lg:!h-[20rem]"
+        placeholder="توضیحات تکمیلی رزرو : "
+      ></textarea>
+    </div>
+  );
+};
+
+export default HotelInformation;
