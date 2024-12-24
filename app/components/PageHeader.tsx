589bbe4bb74c57d912e175db685498c04d41ad56
iman <imankhorshidi72@gmail.com>
2024-12-24 11:04:54 +0330

    Add: PageHeader Component

---
+++ app/components/PageHeader.tsx
@@ -0,0 +1,11 @@
+import SearchInput from "./SearchInput";
+
+const PageHeader = () => {
+  return (
+    <header className="bg-[#262630] h-[145px] flex justify-center pt-3 lg:bg-white">
+      <SearchInput />
+    </header>
+  );
+};
+
+export default PageHeader;
