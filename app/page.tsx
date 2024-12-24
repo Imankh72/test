c5c7b74cee033a4755237a24b16b59d0266b0b84
iman <imankhorshidi72@gmail.com>
2024-12-24 20:44:33 +0330

    Fix: Page Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import PageHeader from "./components/PageHeader";
+import ProgressStatus from "./components/ProgressStatus";
 
 const Home = () => {
   return (
-    <main className="font-inter overflow-hidden bg-gray-tertiary min-h-screen min-w-screen lg:bg-gray-tertiary">
+    <main className="font-vazir overflow-hidden bg-gray-tertiary min-h-screen min-w-screen lg:bg-gray-tertiary flex flex-col gap-5">
       <PageHeader />
+      <ProgressStatus />
     </main>
   );
 };
