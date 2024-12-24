6fd4ea0a5faae967a101ac4175ad4f298cb83b7e
iman <imankhorshidi72@gmail.com>
2024-12-24 11:31:24 +0330

    Fix: PageHeader Component

--- app/components/PageHeader.tsx
+++ app/components/PageHeader.tsx
@@ -1,9 +1,17 @@
 import SearchInput from "./SearchInput";
+import HeroIcon from "./svgs/HeroIcon";
 
 const PageHeader = () => {
   return (
-    <header className="bg-[#262630] h-[145px] flex justify-center pt-3 lg:bg-white">
+    <header className="bg-[#262630] h-36 flex flex-col items-center gap-3 pt-3 lg:bg-white">
       <SearchInput />
+      <div className="flex justify-center items-center gap-2">
+        <HeroIcon />
+        <span className="inline-block bg-[#48486A] h-6 w-[0.125rem] rounded-[11px]"></span>
+        <p className="text-white text-xl tracking-widest font-bold">
+          ZARVANTRIP
+        </p>
+      </div>
     </header>
   );
 };
