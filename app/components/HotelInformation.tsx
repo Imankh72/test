7d0f6336804cde1ed3a0d45b57f89e476f936ed0
iman <imankhorshidi72@gmail.com>
2024-12-25 10:56:42 +0330

    Fix: HotelInformation Component

--- app/components/HotelInformation.tsx
+++ app/components/HotelInformation.tsx
@@ -1,17 +1,17 @@
 const HotelInformation = () => {
   return (
-    <div className="bg-white mx-8 rounded-md py-5 px-4">
-      <div className="py-5 px-4 bg-white rounded-md grid grid-cols-2 grid-rows-4 gap-x-12 gap-y-6 lg:px-8 lg:w-full lg:grid-cols-3 lg:gap-y-12">
+    <div className="bg-white rounded-md py-5 lg:px-4 lg:mx-0">
+      <div className="py-5 px-4 bg-white rounded-md grid grid-cols-2 grid-rows-4 gap-x-12 gap-y-6 lg:px-8 lg:w-full lg:grid-cols-3 lg:gap-y-12 lg:gap-x-40">
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-2">
             <div className="relative">
-              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
                 هتل انتخاب شده :
               </p>
               <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
             </div>
             <div className="relative">
-              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
                 پارسیان آزادی تهران
               </p>
               <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
@@ -21,13 +21,13 @@ const HotelInformation = () => {
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-2">
             <div className="relative">
-              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
                 مدت اقامت :
               </p>
               <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
             </div>
             <div className="relative">
-              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
                 3 شب
               </p>
               <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
@@ -37,13 +37,13 @@ const HotelInformation = () => {
         <div className="flex justify-between items-center row-start-3 row-end-4 col-start-1 col-end-3 lg:col-end-2">
           <div className="flex flex-col gap-2">
             <div className="relative">
-              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
                 مدت اقامت :
               </p>
               <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
             </div>
             <div className="relative">
-              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
                 3 شب
               </p>
               <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
@@ -53,13 +53,13 @@ const HotelInformation = () => {
         <div className="flex justify-between items-center row-start-4 row-end-5 col-start-1 col-end-3">
           <div className="flex flex-col gap-2">
             <div className="relative">
-              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
                 مدت اقامت :
               </p>
               <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
             </div>
             <div className="relative">
-              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
                 3 شب
               </p>
               <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
@@ -69,13 +69,13 @@ const HotelInformation = () => {
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-2">
             <div className="relative">
-              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
                 مدت اقامت :
               </p>
               <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
             </div>
             <div className="relative">
-              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
                 3 شب
               </p>
               <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
@@ -85,13 +85,13 @@ const HotelInformation = () => {
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-2">
             <div className="relative">
-              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
                 مدت اقامت :
               </p>
               <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
             </div>
             <div className="relative">
-              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
                 3 شب
               </p>
               <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
@@ -101,13 +101,13 @@ const HotelInformation = () => {
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-2">
             <div className="relative">
-              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
                 مدت اقامت :
               </p>
               <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
             </div>
             <div className="relative">
-              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
                 3 شب
               </p>
               <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
@@ -117,13 +117,13 @@ const HotelInformation = () => {
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-2">
             <div className="relative">
-              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
                 مدت اقامت :
               </p>
               <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
             </div>
             <div className="relative">
-              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
                 3 شب
               </p>
               <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
@@ -133,13 +133,13 @@ const HotelInformation = () => {
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-2">
             <div className="relative">
-              <p className="text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-dark-primary text-sm font-bold pr-5 lg:text-2xl">
                 مدت اقامت :
               </p>
               <span className="bg-orange-primary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
             </div>
             <div className="relative">
-              <p className="text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
+              <p className="whitespace-nowrap text-gray-primary text-sm font-bold pr-5 lg:text-2xl">
                 3 شب
               </p>
               <span className="bg-gray-secondary rounded-md w-1 h-6 absolute right-0 top-1/2 -translate-y-1/2"></span>
@@ -147,12 +147,14 @@ const HotelInformation = () => {
           </div>
         </div>
       </div>
-      <textarea
-        name=""
-        id=""
-        className="bg-gray-secondary inline-block p-3 w-full placeholder:text-dark-primary placeholder:text-xs placeholder:font-bold resize-none rounded-md lg:!h-[20rem]"
-        placeholder="توضیحات تکمیلی رزرو : "
-      ></textarea>
+      <div className="mx-4 lg:ml-4">
+        <textarea
+          name=""
+          id=""
+          className="bg-gray-secondary inline-block p-3 pl-0 w-full !h-[7.5rem] placeholder:text-dark-primary placeholder::text-xs lg:placeholder:text-xl placeholder:font-bold resize-none rounded-md lg:!h-[20rem] lg:mt-4 lg:mr-3"
+          placeholder="توضیحات تکمیلی رزرو : "
+        ></textarea>
+      </div>
     </div>
   );
 };
